b3a1414cf9184689738fd3db0d62be4f489a7222
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-06 20:57:16 -0500

    connected links

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/home', function (req, res){
     res.sendFile(path.join(__dirname, '/app/public/home.html'));
 });
 
+app.get('/survey', function (req, res){
+    res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+});
 
 
 
