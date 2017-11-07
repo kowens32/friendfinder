31de97abe859d8576065ddecf15d65d212477083
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-07 16:19:51 -0500

    updated html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,23 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
 
 
+module.exports = function(app) {
+//HTML Get Requests
+//Below code handles when users 'visit' a page
+//in each of the below cases the user is shown an HTML page of content
+
+
+
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+    });
+
+    //If no matching route is found default to home
+    app.get('*', function(req,res){
+        res.sendFile(path.join(__dirname,'/app/public/home.html'));
+    });
+};
--- server.js
+++ server.js
@@ -12,22 +12,13 @@ var PORT = 3000;
 
 //Set up the express app to handle data parsing
 //==============================================
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
 
 
 
-//Basic Route that sends the user
-//==========================================
-app.get('/home', function (req, res){
-    res.sendFile(path.join(__dirname, '/app/public/home.html'));
-});
-
-app.get('/survey', function (req, res){
-    res.sendFile(path.join(__dirname, '/app/public/survey.html'));
-});
 
 
 //ROUTER
@@ -35,8 +26,8 @@ app.get('/survey', function (req, res){
 //The routes give our server a 'map' of how to respond when users visit
 //or request data from various URLs
 //==========================================================
-// require('./routing/apiRoutes.js')(app);
-// require('./routing/htmlRoutes.js')(app);
+require('./routing/apiRoutes.js')(app);
+require('./routing/htmlRoutes.js')(app);
 
 
 //LISTENER
