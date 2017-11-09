f97a78e021acfb8e1ee0c88a9373c6c6c5c78229
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-08 20:06:42 -0500

    got the array to store info

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -46,16 +46,4 @@ module.exports = function(app) {
         res.json("Ok");
     });
 
-
-
-
-    app.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname, '/app/public/survey.html'));
-    });
-
-    //If no matching route is found default to home
-    app.get('*', function(req,res){
-        res.sendFile(path.join(__dirname,'/app/public/home.html'));
-    });
-
 };
