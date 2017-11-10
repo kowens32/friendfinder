58d9b6b158931cc4b21bde25787bf7280b0a642e
Michael Daye <mgdaye@gmail.com>
2017-11-10 15:56:17 -0500

    added friends and fixed syntax error on api page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,52 @@
 var friends =[
-
+    {
+        "name": "phil",
+        "photo": "phil.com",
+        "mScores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "eric",
+        "photo": "eric.com",
+        "mScores": [
+            "5",
+            "4",
+            "5",
+            "2",
+            "5",
+            "5",
+            "2",
+            "1",
+            "5",
+            "4"
+        ]
+    },
+    {
+        "name": "jill",
+        "photo": "phil.com",
+        "mScores": [
+            "3",
+            "5",
+            "3",
+            "1",
+            "5",
+            "5",
+            "3",
+            "5",
+            "2",
+            "5"
+        ]
+    }
 ];
 
 module.exports  = friends;
